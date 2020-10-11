435d7f61414354a674860bcf099ff82e21f4a0f8
Lucas Daniel Pedroso <lukas_boo@hotmail.com>
2020-10-11 17:43:27 -0300

    creating httpservice

---
+++ Infra/HttpError.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+    case badRequest
+    case serverError
+    case unauthorized
+    case forbidden
+    case unexpected
+}
