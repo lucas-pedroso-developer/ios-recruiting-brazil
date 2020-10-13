559894f674cfdee3a9b6f7256bf40073e2302dcb
Lucas Daniel Pedroso <lukas_boo@hotmail.com>
2020-10-13 15:08:47 -0300

    installing kingfisher

---
+++ View/Controllers/MainMoviesViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class MainMoviesViewController: UIViewController {
+
+    
+    @IBOutlet weak var moviesCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
